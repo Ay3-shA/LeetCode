5f6f2aaf1fb9c33f04b6a04e776dfd835a88add9
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-08-13 21:19:21 +0500

    New Array File

--- test.cpp
+++ test.cpp
@@ -1,17 +1,21 @@
+//Smallest / Largest of Array
+
 #include<iostream>
 using namespace std;
 
 int main (){
     int nums[] = {5, 15, 22, 1, -15, 24};
     int size = 6;
-    int smallest = INT_MAX;
+    //int smallest = INT_MAX;
+    int largest = INT_MIN;
 
     for(int i=0; i<size; i++){
-        if (nums[i]<smallest){
-            smallest = nums[i];
-        }
+        // if (nums[i]<smallest){
+        //     smallest = nums[i];
+        // }
+        largest = max(nums[i], largest);
     }
-         cout<<smallest;
+         cout<<largest;
 
     return 0;
 }
