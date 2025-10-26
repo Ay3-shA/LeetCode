5fa3100b9463018e17ba949ccdd7f604350a9908
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-10-26 23:13:21 +0500

    star

--- Arrays.cpp
+++
@@ -1,9 +0,0 @@
-// #include<iostream>
-// using namespace std;
-
-// int main(){
-//     cout<<"hello ";
-//     cout<<"Ayesha";
-
-//     return 0;
-// }
---
+++ Star.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m=5, n=10;
+    //no of lines
+    for(int i=0; i<=m; i++){
+            //no of stars
+            for (int j=0; j<=n; j++){
+                cout<<"*";
+            }
+        cout<<endl;
+        }
+return 0;
+}
