db87408e81f145a0bced7d41d1db66afd81c151f
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-10-27 18:55:51 +0500

    122333

---
+++ triangle_122333.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n = 3;
+    for (int i=0; i<n; i++){
+        for (int j=1; j<i+2; j++){
+            cout<<i+1;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
