9dd5543aeeac356f8ce819192f3f1c6e7a71992b
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-10-28 13:36:59 +0500

    inverted triangle pattern

---
+++ Patterns/inverted_triangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    int num=1;
+    //outer loop for no. of lines 
+    for (int i=0;i<n;i++){
+        //print spaces
+        for(int j=0; j<i; j++){
+            cout<<" ";
+        }
+        //print numbers
+        for(int j=0; j<n-i; j++){
+            cout<<(i+1);
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
