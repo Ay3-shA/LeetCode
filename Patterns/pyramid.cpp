19f5a27cec1f92ac28f852ca6ddf8cd7501e32c5
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-10-28 17:13:42 +0500

    pyramid pattern

--- Patterns/pyramid.cpp
+++ Patterns/pyramid.cpp
@@ -5,7 +5,7 @@ int main(){
     //outer loop for no of lines 
     for (int i=0; i<n; i++){
         //spaces
-        for (int j=0; j<n-i;j++){
+        for (int j=0; j<n-i+1;j++){
             cout<<" ";
         }
         //nums1
