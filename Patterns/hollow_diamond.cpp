1947b603300376bff26b4880bbd34647f2de4bcd
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-10-30 21:40:59 +0500

    hollow diamond pattern

--- Patterns/hollow_diamond.cpp
+++ Patterns/hollow_diamond.cpp
@@ -4,7 +4,7 @@ int main(){
     int n = 4;
 
     for (int i=0; i<n; i++){
-        //outer spaces 
+        //TOP
         for(int j=0; j<n-i-1; j++){
             cout<<" ";
         }
@@ -20,6 +20,19 @@ int main(){
         }
                 cout<<endl;
         
+    }
+    for(int i=0; i<n-1;i++){
+        for(int j=0; j<i+1; j++){
+            cout<<" ";
+        }
+        cout<<"*";
+        if(i!=n-2){
+            for(int j=0; j<2*(n-i-2)-1;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
     }
 
     
