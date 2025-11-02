b0b95fd29ca3cc8ca3f995744b29900da831cfc5
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-11-02 20:14:02 +0500

    nth fibonacci series

---
+++ Fuctions/nth_fibonacci.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if (n <= 1){
+        return n;
+    }
+    int prev1=1, prev2=0, curr;
+    for(int i=2;i<n;i++){
+        curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+    }
+    return curr;
+        
+
+}
+
+int main(){
+    int n=7;
+    cout<<fibonacci(n);
+    
+    return 0;
+}
+
+
