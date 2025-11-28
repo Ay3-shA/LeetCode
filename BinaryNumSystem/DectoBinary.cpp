ef6837772a7784342511777f0dd3b438c9e71e3a
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-11-29 01:08:52 +0500

    Decimal to Binary

---
+++ BinaryNumSystem/DectoBinary.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int dectoBinary(int decNum){
+    int ans=0, pow=1;
+
+    while(decNum>0){
+        int rem = decNum%2;
+        decNum /= 2;
+        ans+=(rem*pow);
+        pow*=10;
+    }
+    return ans;
+}
+
+int main(){
+    int decNum=8;
+
+    cout<<dectoBinary(decNum)<<endl;
+
+    return 0;
+}
