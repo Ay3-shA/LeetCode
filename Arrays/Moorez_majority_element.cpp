c8486fa5b8f3d8095cea723e2fb9c96b197eb731
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-12-08 17:25:54 +0500

    Majority element - Moore's voting algorithm

---
+++ Arrays/Moorez_majority_element.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main (){
+    vector<int> nums={1, 2, 2, 1, 1};
+    int n=nums.size();
+
+    int freq=0, ans=nums[0];
+
+    for(int i=0; i<n; i++){
+        if (freq==0){
+            ans=nums[i];
+        }
+        if (ans==nums[i]){
+            freq++;
+        }
+        else{
+            freq--;
+        }
+    }
+    cout<<ans;
+
+    return 0;
+}
+
