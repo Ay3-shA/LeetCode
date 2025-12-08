939588b187753f4cbb901425bcf737f830b1336c
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-12-08 12:53:03 +0500

    majority element brute force approach

---
+++ Arrays/majority_element_brute.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main (){
+    vector<int> nums={1, 2, 2, 1, 1};
+    int n=nums.size();
+    for (int val:nums){
+        int freq=0;
+        for(int el:nums){
+            if(el==val){
+                freq++;
+            }
+        }
+        if(freq>n/2){
+            cout<<val<<" ";
+        }
+    }
+    return 0;
+}
