c1e91ffb1302728fbc0bff62ffac0bfcdfcb6c6c
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-12-09 12:49:47 +0500

    computing x pow n for constraint n = (2^31)-1

---
+++ bitwiseOperators/compute_x_pow_n.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+//x^n - Binary exponentiation
+
+double myPow(double x, int n){
+    long binForm = n;
+    if (n < 0){
+        x = 1/x;
+        binForm = -binForm;
+    }
+    double ans = 1;
+    while(binForm>0){
+        if(binForm%2==1){
+            ans=ans*x;
+        }
+        x= x*x;
+        binForm = binForm/2;
+    }
+    return ans;
+
+}
+
+int main(){
+
+    cout<<myPow(3, 5);
+    return 0;
+}
