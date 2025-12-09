5c7a8cb5815ce77dd344055951604bf2984c5ccd
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-12-09 23:20:37 +0500

    binary search

---
+++ vectors/binary_search.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch (vector<int>& arr1, int n, int target){
+    int start=0, end=n-1;
+    while (start<=end){
+        int mid = (start + end)/2;
+        if(target > arr1[mid]){
+            start = mid + 1;
+        }
+        else if(target<arr1[mid]){
+            end=mid-1;
+        }
+        else{
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main(){
+
+    vector<int> arr1 = {-1, 0, 3, 4, 5, 8, 9 ,12};
+    int n=arr1.size();
+    int target = 9;
+
+    cout<<binarySearch(arr1, n, target);
+    return 0;
+}
