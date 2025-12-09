6d430cc27ec8d314039d5e4c75b974ba71b916d4
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-12-09 23:30:15 +0500

    optimized binary search

--- vectors/binary_search.cpp
+++ vectors/binary_search.cpp
@@ -6,13 +6,13 @@ int binarySearch (vector<int>& arr1, int target){
     int n=arr1.size();
     int start=0, end=n-1;
     while (start<=end){
-        int mid = (start + end)/2;
+        int mid = (start + ((end-start)/2));
         if(target > arr1[mid]){
             start = mid + 1;
         }
         else if(target<arr1[mid]){
             end=mid-1;
-        }
+        } 
         else{
             return mid;
         }
