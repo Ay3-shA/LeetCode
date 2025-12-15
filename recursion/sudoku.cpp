585d3e989f1a147b002c311e0d4fe7dab76060cb
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-12-15 16:41:21 +0500

    sudoku solver problem

---
+++ recursion/sudoku.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(vector<vector<char>>& board, int row, int col, char dig){
+    //horizontal
+    for(int j=0; j<9; j++){
+        if (board[row][j] == dig){
+            return false;
+        }
+    }
+
+    //vertical
+    for(int i=0; i<9; i++){
+        if (board[i][col] == dig){
+            return false;
+        }
+    }
+
+    //grid
+
+    int srow=(row/3)*3;
+    int scol=(col/3)*3;
+    for (int i=srow; i<=srow+2; i++){
+        for (int j=scol; j<=scol+2; j++){
+            if (board[i][j] == dig){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool helper(vector<vector<char>>& board, int row, int col){
+
+    //base case
+    if (row==9){
+        return true;
+    }
+
+    int nextRow=row, nextCol=col+1;
+    if(nextCol==9){
+        nextRow=row+1;
+        nextCol=0;
+    }
+     
+    if (board[row][col]!='.'){
+        return helper(board, nextRow, nextCol);
+    }
+
+    //place the digit
+    for (char dig='1'; dig<='9'; dig++){
+        if (isSafe(board, row, col, dig)){
+            board[row][col] = dig;
+            if (helper(board, nextRow, nextCol)){
+                return true;
+            }else{
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    return false;
+}
+
+void solvesudoku(vector<vector<char>>& board){
+    helper(board, 0, 0);
+}
+
+void printBoard(vector<vector<char>>& board) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main (){
+    vector<vector<char>> board = {
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}
+    };
+    solvesudoku(board);
+    printBoard(board);
+
+    return 0;
+}
+
