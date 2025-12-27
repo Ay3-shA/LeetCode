6adbe33b2fadb3d1ab21bd450fb197cb9e439af0
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-12-27 19:21:12 +0500

    cycle detection in bfs

---
+++ Graphs/cycle_detection_bfs.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<queue>
+using namespace std;
+
+class Graph{
+    int V;
+    //dynamic array
+    list<int> *l; //int *arr
+    public:
+    //constructor
+    Graph(int V){
+        this->V = V;
+        l = new list<int> [V];
+        //arr = new int[V]
+    }
+
+    void addEdge(int u, int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+
+    bool isCycleUndirBFS(int src, vector<bool>& vis){
+        queue<pair<int, int>> q;
+        q.push({src, -1});
+
+        vis[src] = true;
+
+        while(q.size()>0){
+            int u = q.front().first;
+            int parU = q.front().second;
+            q.pop();
+
+            list<int> neighbors = l[u];
+            for (int v: neighbors){
+                if (!vis[v]){
+                    q.push({v, u}); // u------v
+                    vis[v] = true;
+                }
+                else if (v != parU){ // cycle condition
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool isCycle(){
+        vector<bool> vis(V, false);
+        for(int i=0; i<V; i++){
+            if (!vis[i]){
+                if (isCycleUndirBFS(i, vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+};
+
+int main(){
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 3);
+    g.addEdge(2, 4);
+
+    cout<<g.isCycle();
+
+    return 0;
+}
