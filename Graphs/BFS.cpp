5422919f59be5fb26b1d6444dd00045eec9e39ce
Ay3-shA <ayeshakanwal2468@gmail.com>
2025-12-28 02:12:07 +0500

    example test

--- Graphs/BFS.cpp
+++ Graphs/BFS.cpp
@@ -49,12 +49,23 @@ class Graph{
 };
 
 int main(){
-    Graph g(5);
+    Graph g(7);
     g.addEdge(0, 1);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(2, 3);
+    g.addEdge(0, 2);
+    g.addEdge(0, 4);
+    g.addEdge(1, 0);
+    g.addEdge(2, 0);
     g.addEdge(2, 4);
+    g.addEdge(2, 5);
+    g.addEdge(2, 3);
+    g.addEdge(3, 2);
+    g.addEdge(3, 6);
+    g.addEdge(4, 0);
+    g.addEdge(4, 2);
+    g.addEdge(4, 5);
+    g.addEdge(5, 2);
+    g.addEdge(5, 4);
+    g.addEdge(6, 3);
 
     g.bfs();
 
