3e7c0eac02cc27e5f941a34095bc2f498d8c434a
Ay3-shA <ayeshakanwal2468@gmail.com>
2026-01-11 04:49:06 +0500

    post order traversal

---
+++ BinaryTrees/postOrder_Traversal.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    //constructor 
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> preOrder){
+    idx++;
+
+    //Base case
+    if(preOrder[idx]==-1){
+        return NULL;
+    }
+
+    Node* root = new Node(preOrder[idx]);
+
+    root->left = buildTree(preOrder); //LEFT SUBTREE
+    root->right = buildTree(preOrder); //RIGHT SUBTREE
+
+    return root;
+}
+
+//preOrder Traversal
+
+void postOrderTrav(Node* root){
+
+    //base case
+    if (root==NULL){
+        return;
+    }
+
+    postOrderTrav(root->left);
+    postOrderTrav(root->right);
+    cout<<root->data<<" "<<endl;
+
+}
+
+int main(){
+    vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = buildTree(preOrder);
+    postOrderTrav(root);
+
+    cout<<endl;
+
+    return 0;
+}
