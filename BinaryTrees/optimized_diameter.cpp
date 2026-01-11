ea83d09ac6d69f7ebdafa88481af74ec3e404507
Ay3-shA <ayeshakanwal2468@gmail.com>
2026-01-11 23:15:25 +0500

    optimized diameter of binary tree

---
+++ BinaryTrees/optimized_diameter.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    //constructor 
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> preOrder){
+    idx++;
+
+    //Base case
+    if(preOrder[idx]==-1){
+        return NULL;
+    }
+
+    Node* root = new Node(preOrder[idx]);
+
+    root->left = buildTree(preOrder); //LEFT SUBTREE
+    root->right = buildTree(preOrder); //RIGHT SUBTREE
+
+    return root;
+}
+
+//height of a tree
+int ans = 0;
+int height(Node* root){
+    //base case
+    if(root == NULL){
+        return 0;
+    }
+
+    int left_height = height(root->left);
+    int right_height = height(root->right);
+
+    ans = max (ans, left_height+right_height);
+
+    return max(left_height, right_height)+1;
+}
+
+int diameter(Node* root){
+    height(root);
+
+    return ans;
+}
+
+int main(){
+    vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = buildTree(preOrder);
+    cout<<"Diameter of Binary Tree : "<<diameter(root);
+
+    return 0;
+}
