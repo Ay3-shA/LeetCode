00810e306f320035220aa744665d16ff6d27d2fb
Ay3-shA <ayeshakanwal2468@gmail.com>
2026-01-11 16:29:04 +0500

    count of nodes

---
+++ BinaryTrees/count_of_nodes.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    //constructor 
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* buildTree(vector<int> preOrder){
+    idx++;
+
+    //Base case
+    if(preOrder[idx]==-1){
+        return NULL;
+    }
+
+    Node* root = new Node(preOrder[idx]);
+
+    root->left = buildTree(preOrder); //LEFT SUBTREE
+    root->right = buildTree(preOrder); //RIGHT SUBTREE
+
+    return root;
+}
+
+//count of nodes
+int count(Node* root){
+    //base case
+    if(root == NULL){
+        return 0;
+    }
+
+    int left_count = count(root->left);
+    int right_count = count(root->right);
+
+    return left_count + right_count+1;
+}
+
+int main(){
+    vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = buildTree(preOrder);
+    cout<<"count : "<<count(root);
+
+    return 0;
+}
