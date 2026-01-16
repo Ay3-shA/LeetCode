852c4d8d5165c0bdaf509202539bb8c5a5c044b6
Ay3-shA <ayeshakanwal2468@gmail.com>
2026-01-16 18:52:55 +0500

    kth level of a binary tree

--- BinaryTrees/kthLevel.cpp
+++ BinaryTrees/kthLevel.cpp
@@ -28,6 +28,7 @@ Node* buildTree(vector<int> preOrder){
         return NULL;
     }
 
+
     Node* root = new Node(preOrder[idx]);
 
     root->left = buildTree(preOrder); //LEFT SUBTREE
@@ -37,43 +38,27 @@ Node* buildTree(vector<int> preOrder){
 
 }
 
-//bottom view of a binary tree
-void bottomView(Node* root){
-
-    if(root == NULL){
+//kth level of a binary tree
+void kthLevel(Node* root, int K){
+    //base case
+    if(root==NULL){
+        return;
+    }
+    if(K==1){
+        cout<<root->data<<" ";
         return;
     }
 
-    queue<pair<Node*, int>> q;
-    map<int, int> m;
-    q.push({root, 0});
+    kthLevel(root->left, K-1);
+    kthLevel(root->right, K-1);
 
-    while(q.size()>0){
-        Node* curr = q.front().first;
-        int currHD = q.front().second;
-        q.pop();
-
-        m[currHD] = curr->data;
-
-        if(curr->left!=NULL){
-            q.push({curr->left, currHD-1});
-        }
-        if(curr->right!=NULL){
-            q.push({curr->right, currHD+1});
-        }
-    }
-
-    for(auto it : m){
-        cout<<it.second<<" ";
-    }
 }
 
 int main(){
-    vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
-
+    vector<int> preOrder = {1, 2, 7, -1, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node* root = buildTree(preOrder);
 
-    
+    kthLevel(root, 3);
 
     return 0;
 }
